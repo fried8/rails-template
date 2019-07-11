3c3839eb26255cf72459e47b04ae521acf845172
Dat Le-Tien <vnkeymaster@gmail.com>
2019-07-11 13:47:22 +0700

    Add routes in rails that synced up with react-router

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/", action: :index, controller: "home"
+  get "/blog-overview", action: :index, controller: "home"
+  get "/user-profile-lite", action: :index, controller: "home"
+  get "/add-new-post", action: :index, controller: "home"
+  get "/errors", action: :index, controller: "home"
+  get "/components-overview", action: :index, controller: "home"
+  get "/tables", action: :index, controller: "home"
+  get "/blog-posts", action: :index, controller: "home"
 end
